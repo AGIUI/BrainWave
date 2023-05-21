b35a23332135fc92ec96f982b864e4b3ebf808f3
shadowcz007 <chizhiwei007@163.com>
2023-05-21 17:43:39 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# BrainWave
-BrainWave is an innovative and intelligent suggestion editor.
+# 
+
+
+## Installation
+
+```sh
+npm install
+```
+
+### Start Dev Server
+
+```sh
+npm run dev
+```
+
+Runs the app in development mode. Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+### Build
+
+```sh
+npm run build
+```
---
+++ src/components/Flow/index.tsx
@@ -0,0 +1,107 @@
+import { useCallback } from 'react';
+import ReactFlow, {
+  Node,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Connection,
+  Edge,
+  ConnectionLineType,
+  
+  MiniMap,
+  Controls,
+  Background,
+} from 'reactflow';
+import CustomNode from './CustomNode';
+
+import styles from './Flow.module.css';
+
+const minimapStyle = {
+  height: 120,
+};
+
+const initialNodes: Node[] = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'Node 1' },
+    position: { x: 250, y: 5 },
+  },
+  {
+    id: '2',
+    data: { label: 'Node 2' },
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: '3',
+    data: { label: 'Node 3' },
+    position: { x: 400, y: 100 },
+  },
+  {
+    id: '4',
+    data: { label: 'Node 4' },
+    position: { x: 400, y: 200 },
+    type: 'custom',
+    className: styles.customNode,
+  }, {
+    id: '7',
+    type: 'default',
+    className: 'annotation',
+    data: {
+      label: (
+        <>
+          On the bottom left you see the <strong>Controls</strong> and the bottom right the{' '}
+          <strong>MiniMap</strong>. This is also just a node 🥳
+        </>
+      ),
+    },
+    draggable: false,
+    selectable: false,
+    position: { x: 150, y: 400 },
+  },
+];
+
+const initialEdges: Edge[] = [
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e1-3', source: '1', target: '3' },
+];
+
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+const defaultEdgeOptions = {
+  animated: true,
+  type: 'smoothstep',
+};
+
+function Flow() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params: Connection | Edge) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div className={styles.flow}>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        defaultEdgeOptions={defaultEdgeOptions}
+        connectionLineType={ConnectionLineType.SmoothStep}
+        fitView
+      >
+      <MiniMap style={minimapStyle} zoomable pannable />
+      <Controls />
+      <Background color="#aaa" gap={16} />
+    </ReactFlow>
+    </div>
+  );
+}
+
+export default Flow;
